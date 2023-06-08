dba1c44a98359e17223e937bfca122ae65e71798
Quitaxd <108627333+Quitaxd@users.noreply.github.com>
2023-06-08 11:46:17 +0300

    Delete hyprland directory

--- hyprland/start-in-tray.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# start waybar and programs with tray icons after pause
-waybar &
-sleep 1
-nm-applet --indicator &
-crow &
-syncthingtray --wait &
-XDG_CURRENT_DESKTOP=gnome telegram-desktop &
