a7a165f0df9e5c59a17bf88d29c559a8cc93b02e
Quitaxd <108627333+Quitaxd@users.noreply.github.com>
2023-06-08 15:57:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 cd ~/.dotfiles
 cp -r * ~/.config
-paru -S hyprland-nvidia-git waybar-hyprland-git hyprpaper alacritty zsh 
+paru -S hyprland-nvidia-git waybar-hyprland-git hyprpaper alacritty zsh wofi 
