f81a36a3943ce2f91e4bdcee26ac6a54ac65efd6
Quitaxd <quitaxd@tuta.io>
2023-06-08 11:43:37 +0300

    repo

---
+++ i3/locker.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# exit fullscreen from mpv before lock screen
+/usr/bin/xdotool key \
+	--clearmodifiers \
+	--window "$(/usr/bin/xdotool search --onlyvisible --class mpv)" Escape
+
+# sleep
+if [ "$1" = "sleep" ]; then
+	systemctl suspend &
+fi
+
+# locker
+XSECURELOCK_COMPOSITE_OBSCURER=0 XSECURELOCK_SHOW_USERNAME=0 \
+	XSECURELOCK_SHOW_HOSTNAME=0 XSECURELOCK_SHOW_DATETIME=1 \
+	XSECURELOCK_PASSWORD_PROMPT=kaomoji xsecurelock
