9905760207c86eafec867b147237fd4e176f3533
Quitaxd <108627333+Quitaxd@users.noreply.github.com>
2023-06-09 22:45:47 +0300

    Add xfce terminal and copy old files to ~/.config-backup

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 cd ~/.dotfiles
+mkdir ~/.config-backup
+mv ~/.config/hypr/ ~/.config-backup
+mv ~/.config/alacritty/ ~/.config-backup
+mv ~/.config/i3/ ~/.config-backup
+mv ~/.config/polybar/ ~/.config-backup
+mv ~/.config/waybar/ ~/.config-backup
+mv ~/.config/wofi/ ~/.config-backup
+mv ~/.config/picom.conf ~/.config-backup
 cp -r * ~/.config
-paru -S hyprland-nvidia-git waybar-hyprland-git hyprpaper alacritty zsh wofi wlogout
+paru -S hyprland-nvidia-git waybar-hyprland-git hyprpaper alacritty zsh wofi wlogout xfce4-terminal 
 cd
