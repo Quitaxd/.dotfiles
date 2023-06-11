c4aedd6a52166a623605c7376c347c7bef77a752
Quitaxd <108627333+Quitaxd@users.noreply.github.com>
2023-06-11 15:11:10 +0300

    Create install_fedora.sh

---
+++ install_fedora.sh
@@ -0,0 +1,24 @@
+cd ~/.dotfiles
+rm -rf ~/.config-backup
+mkdir ~/.config-backup
+mv ~/Wallpapers ~/.Wallpapers-Backup
+mkdir ~/Wallpapers
+cp wp.jpg ~/Wallpapers/wp.jpg
+mv ~/.config/hypr/ ~/.config-backup
+mv ~/.config/alacritty/ ~/.config-backup
+mv ~/.config/i3/ ~/.config-backup
+mv ~/.config/polybar/ ~/.config-backup
+mv ~/.config/waybar/ ~/.config-backup
+mv ~/.config/wofi/ ~/.config-backup
+mv ~/.config/picom.conf ~/.config-backup
+cp -r * ~/.config
+
+sudo dnf install ninja-build cmake meson gcc-c++ libxcb-devel libX11-devel pixman-devel wayland-protocols-devel cairo-devel pango-devel
+git clone --recursive https://github.com/hyprwm/Hyprland
+cd Hyprland
+meson _build
+ninja -C _build
+sudo ninja -C _build install
+sudo cp /usr/local/share/wayland-sessions/hyprland.desktop /usr/share/wayland-sessions
+
+echo "Check /etc/gdm/custom.conf and make sure Wayland is enabled."
