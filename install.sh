bd602f08ac52149632583bf1831b8f109d60875c
Quitaxd <108627333+Quitaxd@users.noreply.github.com>
2023-06-11 12:36:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,6 +22,6 @@ cd ~/.dotfiles
 paru -S hyprland-nvidia-git waybar-hyprland-git hyprpaper alacritty zsh wofi rofi rofi-emoji wlogout xfce4-terminal xdg-desktop-portal-hyprland xdg-desktop-portal pipewire wireplumber polkit-kde-agent qt5-wayland qt6-wayland mako hyprpaper foot thunar wlogout grimblast ksysguard hyprpicker --noconfirm
 
 # i3 Apps
-paru -S autotiling nm-applet polybar picom arcolinux-logout pavucontrol xorg-xkill spectacle -r volumeicon dmenu --noconfirm
+paru -S autotiling nm-applet polybar picom arcolinux-logout pavucontrol xorg-xkill spectacle -r volumeicon dmenu i3 --noconfirm
 
 cd ~/.dotfiles
