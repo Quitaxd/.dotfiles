9e288e5b9df23ab253bc093021a2f64487966fd2
Quitaxd <108627333+Quitaxd@users.noreply.github.com>
2023-06-11 15:14:46 +0300

    Update install_fedora.sh

--- install_fedora.sh
+++ install_fedora.sh
@@ -13,12 +13,12 @@ mv ~/.config/wofi/ ~/.config-backup
 mv ~/.config/picom.conf ~/.config-backup
 cp -r * ~/.config
 
-sudo dnf install ninja-build cmake meson gcc-c++ libxcb-devel libX11-devel pixman-devel wayland-protocols-devel cairo-devel pango-devel
+sudo dnf install ninja-build cmake meson gcc-c++ libxcb-devel libX11-devel pixman-devel wayland-protocols-devel cairo-devel pango-devel -y
 git clone --recursive https://github.com/hyprwm/Hyprland
 cd Hyprland
 meson _build
 ninja -C _build
 sudo ninja -C _build install
-sudo cp /usr/local/share/wayland-sessions/hyprland.desktop /usr/share/wayland-sessions
+sudo cp /usr/local/share/wayland-sessions/hyprland.desktop /usr/share/wayland-sessions/
 
 echo "Check /etc/gdm/custom.conf and make sure Wayland is enabled."
