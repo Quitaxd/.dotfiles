9621756cbaa2cd1981d20cfa13b18bd19b2efcfb
Quitaxd <108627333+Quitaxd@users.noreply.github.com>
2023-06-11 15:12:10 +0300

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -25,7 +25,4 @@ cd ~/.dotfiles
 # Hyprland Apps
 paru -S hyprland-nvidia-git waybar-hyprland-git hyprpaper alacritty zsh wofi rofi rofi-emoji wlogout xfce4-terminal xdg-desktop-portal-hyprland xdg-desktop-portal pipewire wireplumber polkit-kde-agent qt5-wayland qt6-wayland mako hyprpaper foot thunar wlogout grimblast ksysguard hyprpicker --noconfirm
 
-# i3 Apps
-paru -S autotiling nm-applet polybar picom arcolinux-logout pavucontrol xorg-xkill spectacle -r volumeicon dmenu i3 --noconfirm
-
 cd ~/.dotfiles
