0ed3ad266bd6d0bdd6ab8b4407b6939bdcf6b220
Quitaxd <108627333+Quitaxd@users.noreply.github.com>
2023-07-02 14:12:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,6 @@ rm ~/.config/install.sh
 
 
 # Hyprland Apps
-paru -S hyprpaper alacritty zsh wofi rofi rofi-emoji wlogout xfce4-terminal xdg-desktop-portal-hyprland xdg-desktop-portal pipewire wireplumber polkit-kde-agent qt5-wayland qt6-wayland mako hyprpaper foot thunar wlogout grimblast ksysguard hyprpicker nitch --noconfirm
+paru -S hyprpaper alacritty zsh wofi rofi rofi-emoji wlogout xfce4-terminal xdg-desktop-portal-hyprland xdg-desktop-portal pipewire wireplumber polkit-kde-agent qt5-wayland qt6-wayland mako hyprpaper foot thunar grimblast ksysguard hyprpicker nitch --noconfirm
 
 cd ~/.dotfiles
